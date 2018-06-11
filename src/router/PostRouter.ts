a673408ddf2637634e775a84c99ce56b13644d55
Clayton Ray <iamclaytonray@gmail.com>
2018-06-11 01:53:41 -0400

    chore: formatting with routers

--- src/router/PostRouter.ts
+++ src/router/PostRouter.ts
@@ -2,7 +2,6 @@ import { Request, Response, Router } from 'express';
 import Post from '../models/Post';
 
 export class PostRouter {
-
   public router: Router;
 
   constructor() {
@@ -13,39 +12,37 @@ export class PostRouter {
   // get all of the posts in the database
   public all(req: Request, res: Response): void {
     Post.find()
-    .then((data) => {
-      res.status(200).json({ data });
-    })
-    .catch((error) => {
-      res.json({ error });
-    });
+      .then((data) => {
+        res.status(200).json({ data });
+      })
+      .catch((error) => {
+        res.json({ error });
+      });
   }
 
   // get a single post by params of 'slug'
   public one(req: Request, res: Response): void {
-    const slug: string = req.params.slug;
-    
-    Post.findOne({slug})
-    .then((data) => {
-      res.status(200).json({ data });
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+    const { slug } = req.params;
+
+    Post.findOne({ slug })
+      .then((data) => {
+        res.status(200).json({ data });
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
   }
 
   // create a new post
   public create(req: Request, res: Response): void {
-    const title: string = req.body.title;
-    const slug: string = req.body.slug;
-    const content: string = req.body.content;
-    const featuredImage: string = req.body.featuredImage;
-    const category: string = req.body.category;
-    const published: boolean = req.body.published;
-
-    if (!title || !slug || !content) {
-      res.status(422).json({ message: 'All Fields Required.' });
-    }
+    const {
+      title,
+      slug,
+      content,
+      featuredImage,
+      category,
+      published,
+    } = req.body;
 
     const post = new Post({
       title,
@@ -53,42 +50,43 @@ export class PostRouter {
       content,
       featuredImage,
       category,
-      published
+      published,
     });
 
-    post.save()
-    .then((data) => {
-      res.status(201).json({ data });
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+    post
+      .save()
+      .then((data) => {
+        res.status(201).json({ data });
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
   }
 
   // update post by params of 'slug'
   public update(req: Request, res: Response): void {
-    const slug: string = req.body.slug;
+    const { slug } = req.body;
 
-    Post.findOneAndUpdate({slug}, req.body)
-    .then((data) => {
-      res.status(200).json({ data });
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+    Post.findOneAndUpdate({ slug }, req.body)
+      .then((data) => {
+        res.status(200).json({ data });
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
   }
 
   // delete post by params of 'slug'
   public delete(req: Request, res: Response): void {
-    const slug: string = req.body.slug;
+    const { slug } = req.body;
 
-    Post.findOneAndRemove({slug})
-    .then(() => {
-      res.status(204).end();
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+    Post.findOneAndRemove({ slug })
+      .then(() => {
+        res.status(204).end();
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
   }
 
   public routes() {
@@ -98,7 +96,6 @@ export class PostRouter {
     this.router.put('/:slug', this.update);
     this.router.delete('/:slug', this.delete);
   }
-
 }
 
 const postRoutes = new PostRouter();
--- src/router/UserRouter.ts
+++ src/router/UserRouter.ts
@@ -2,7 +2,6 @@ import { Request, Response, Router } from 'express';
 import User from '../models/User';
 
 class UserRouter {
-
   public router: Router;
 
   constructor() {
@@ -11,78 +10,71 @@ class UserRouter {
   }
 
   public all(req: Request, res: Response): void {
-    
     User.find()
-    .then((data) => {
-      res.status(200).json({ data });
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
-  
+      .then((data) => {
+        return res.status(200).json({ data });
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+        return error;
+      });
   }
 
   public one(req: Request, res: Response): void {
-    const username: string = req.params.username;
+    const { username } = req.params;
 
     User.findOne({ username })
-    .then((data) => {
-      res.status(200).json({ data });
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+      .then((data) => {
+        res.status(200).json({ data });
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
   }
 
   public create(req: Request, res: Response): void {
-    const firstName: string = req.body.firstName;
-    const lastName: string = req.body.lastName;
-    const username: string = req.body.username;
-    const email: string = req.body.email;
-    const password: string = req.body.password;
+    const { firstName, lastName, username, email, password } = req.body;
 
     const user = new User({
       firstName,
       lastName,
       username,
       email,
-      password
+      password,
     });
 
-    user.save()
-    .then((data) => {
-      res.status(201).json({ data });
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
-
+    user
+      .save()
+      .then((data) => {
+        res.status(201).json({ data });
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
   }
 
   public update(req: Request, res: Response): void {
-    const username: string = req.params.username;
+    const { username } = req.params;
 
     User.findOneAndUpdate({ username }, req.body)
-    .then((data) => {
-      res.status(200).json({ data });
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
-
+      .then((data) => {
+        res.status(200).json({ data });
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
   }
 
   public delete(req: Request, res: Response): void {
-    const username: string = req.params.username;
+    const { username } = req.params;
 
     User.findOneAndRemove({ username })
-    .then(() => {
-      res.status(204).end();
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
-
+      .then(() => {
+        res.status(204).end();
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
   }
 
   // set up our routes
@@ -93,7 +85,6 @@ class UserRouter {
     this.router.put('/:username', this.update);
     this.router.delete('/:username', this.delete);
   }
-
 }
 
 const userRoutes = new UserRouter();
